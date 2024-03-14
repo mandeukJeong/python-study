a491b645a206df1df43d7bc5e903b8062bb73161
mandeukJeong <wjdalstj137@gmail.com>
2024-03-14 14:04:17 +0900

    ch03 - 조건문: Bool

---
+++ condition/boolean.py
@@ -0,0 +1,10 @@
+x = 10
+under_20 = x < 20
+print("under_20", under_20)
+print("not under_20", not under_20)
+
+print(True and True)
+print(True and False)
+print(True or True)
+print(False or True)
+print(False or False)
