4f673cdb284c697c4e428ee773c49823ae0e8a77
mandeukJeong <wjdalstj137@gmail.com>
2024-03-17 23:08:03 +0900

    ch03 - 조건문: Not Implemented

---
+++ condition/not_implemented.py
@@ -0,0 +1,9 @@
+number = input("정수 입력> ")
+number = int(number)
+
+if number > 0:
+    # 양수일 때: 아직 미구현 상태입니다.
+    raise NotImplementedError
+else:
+    # 음수일 때: 아직 미구현 상태입니다.
+    raise NotImplementedError
